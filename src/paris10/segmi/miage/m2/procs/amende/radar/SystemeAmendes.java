21a12427784911842feeade70e79f0de99eb5b1d
mohamed <momo-du_78@hotmail.fr>
2017-01-25 17:41:00 +0100

    Ajout du projet Amende

---
+++ src/paris10/segmi/miage/m2/procs/amende/radar/SystemeAmendes.java
@@ -0,0 +1,9 @@
+package paris10.segmi.miage.m2.procs.amende.radar;
+
+/**
+ * Created by mohamed on 25/01/2017.
+ */
+public interface SystemeAmendes {
+
+
+}
---
+++ src/paris10/segmi/miage/m2/procs/amende/radar/SystemeAmendesImpl.java
@@ -0,0 +1,7 @@
+package paris10.segmi.miage.m2.procs.amende.radar;
+
+/**
+ * Created by mohamed on 25/01/2017.
+ */
+public class SystemeAmendesImpl {
+}
